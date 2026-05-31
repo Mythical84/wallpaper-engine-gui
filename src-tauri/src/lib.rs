44b15f4127fb4dcef4d9437775027a2f53e501ee
Tyler <artinger.tb@gmail.com>
2026-05-31 19:40:10 -0400

    Made window be destroyed and recreated when closing/showing, reduces background ram usage and fixes bug related to title bar

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,9 +1,10 @@
+use std::process::exit;
 use std::time::Duration;
 
 use display_info::DisplayInfo;
 use tauri::menu::{Menu, MenuItem};
 use tauri::tray::TrayIconBuilder;
-use tauri::{AppHandle, Emitter, Manager};
+use tauri::{AppHandle, Emitter, Manager, WebviewWindowBuilder};
 
 #[tauri::command]
 fn get_monitors() -> Vec<String> {
@@ -37,7 +38,7 @@ fn start_monitor_watcher(app: AppHandle) {
 }
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
-pub fn run() {
+pub fn run() -> Result<(), Box<dyn std::error::Error>> {
     tauri::Builder::default()
         .plugin(tauri_plugin_dialog::init())
         .setup(|app| {
@@ -50,9 +51,19 @@ pub fn run() {
                 .menu(&menu)
                 .icon(app.default_window_icon().unwrap().clone())
                 .on_menu_event(|app, event| match event.id.as_ref() {
-                    "quit" => app.exit(0),
+                    "quit" => exit(0),
                     "show" => {
-                        app.webview_windows().get("main").unwrap().show().unwrap();
+                        if app.get_webview_window("main") == None {
+                            WebviewWindowBuilder::new(
+                                app,
+                                "main",
+                                tauri::WebviewUrl::App("index.html".into())
+                            )
+                            .title("wallpaper engine gui")
+                            .inner_size(1200.0, 900.0)
+                            .build()
+                            .unwrap();
+                        }
                     }
                     _ => {
                         panic!("Unsupported menu option")
@@ -65,12 +76,17 @@ pub fn run() {
         .plugin(tauri_plugin_fs::init())
         .on_window_event(|window, event| match event {
             tauri::WindowEvent::CloseRequested { api, .. } => {
+                window.destroy().unwrap();
                 api.prevent_close();
-                window.hide().unwrap();
             }
             _ => {}
         })
         .invoke_handler(tauri::generate_handler![get_monitors])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        .build(tauri::generate_context!())?
+        .run(|_, event| {
+            if let tauri::RunEvent::ExitRequested { api, .. } = event {
+                api.prevent_exit();
+            }
+        });
+    Ok(())
 }
